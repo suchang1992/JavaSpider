172b57a11d5aa6e6268116c51cf41236cf5108af
suchang1992 <549917764@qq.com>
2015-03-02 15:04:05 +0800

    添加Thread

---
+++ src/main/java/com/hirebigdata/spider/ThreadClass/ThreadUserSkill_Topic.java
@@ -0,0 +1,46 @@
+package com.hirebigdata.spider.ThreadClass;
+
+import com.hirebigdata.spider.pojo.Skilled_topic;
+import com.hirebigdata.spider.pojo.ZhihuUserSkill_Topic;
+import com.hirebigdata.spider.utils.Mongo;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created by Administrator on 2015/1/15.
+ */
+public class ThreadUserSkill_Topic implements Runnable{
+    ZhihuUserSkill_Topic zhihuUserSkill_topic = new ZhihuUserSkill_Topic();
+    String html;
+    String User_data_id;
+
+    public ThreadUserSkill_Topic(String html, String user_data_id) {
+        this.html = html;
+        User_data_id = user_data_id;
+    }
+
+    @Override
+    public void run() {
+        Document page = Jsoup.parse(html);
+        Element e = page.getElementsByAttributeValue("class","zm-profile-section-list zg-clear").first();
+        if (e != null){
+            Elements els = e.getElementsByAttributeValue("class", "item");
+            for ( Element element : els){
+                Skilled_topic skilled_topic = new Skilled_topic();
+                skilled_topic.setName(element.getElementsByAttributeValue("class", "zg-gray-darker").text());
+                skilled_topic.setUrl(element.attr("data-url-token"));
+                skilled_topic.setVote(element.getElementsByAttributeValue("class","zg-icon vote").first().parent().text());
+                skilled_topic.setComment(element.getElementsByAttributeValue("class","zg-icon comment").first().parent().text());
+                zhihuUserSkill_topic.getSkilled_topics().add(skilled_topic);
+            }
+            new Mongo().upsertUserSkill(User_data_id,zhihuUserSkill_topic);
+            return ;
+        }else {
+            return ;
+        }
+    }
+}
---
+++ src/main/java/com/hirebigdata/spider/ThreadClass/ThreadUserTopic.java
@@ -0,0 +1,62 @@
+package com.hirebigdata.spider.ThreadClass;
+
+import com.hirebigdata.spider.main.Spider;
+import com.hirebigdata.spider.pojo.Topic;
+import com.hirebigdata.spider.pojo.ZhihuUserTopic;
+import com.hirebigdata.spider.utils.HttpUtil;
+import com.hirebigdata.spider.utils.Mongo;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by Administrator on 2015/1/13.
+ */
+public class ThreadUserTopic implements Runnable {
+    ZhihuUserTopic zhihuUserTopic = new ZhihuUserTopic();
+    String User_data_id;
+    String User_Url_name;
+
+    public ThreadUserTopic(String user_data_id, String user_Url_name) {
+        User_data_id = user_data_id;
+        User_Url_name = user_Url_name;
+    }
+
+    @Override
+    public void run() {
+        int flag = getTopicDetil(User_Url_name, zhihuUserTopic);
+        if (flag == -1)
+            flag = getTopicDetil(User_Url_name, zhihuUserTopic);
+
+        new Mongo().upsertUserTopic(User_data_id, zhihuUserTopic);
+        return ;
+    }
+
+    private int getTopicDetil(String User_Url_name, ZhihuUserTopic zhihuUserTopic){
+        String html = null;
+        try {
+            html = new HttpUtil().get("http://www.zhihu.com/people/" + User_Url_name + "/topics", Spider.getHeader());
+            if (html.equals("404")) {
+                return -1;
+            }
+            Document page = Jsoup.parse(html);
+            Elements els= page.getElementById("zh-profile-topic-list").getElementsByAttributeValue("class","zm-profile-section-main");
+            for(Element e : els){
+                Topic topic = new Topic();
+                topic.setUrl(e.getElementsByTag("a").get(1).attr("href"));
+                topic.setName(e.getElementsByTag("a").get(1).text());
+                topic.setContent(e.getElementsByAttributeValue("class", "zm-editable-content").first().text());
+                topic.setAnswers_count(e.getElementsByAttributeValue("class", "zg-link-gray").first().text());
+                zhihuUserTopic.getTopics().add(topic);
+            }
+            return 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+}
