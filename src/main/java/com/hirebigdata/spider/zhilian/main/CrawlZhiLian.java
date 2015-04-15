b6036424e295ea86baa2093ab605061ef9613e85
shellbye <shellbye.com@gmail.com>
2015-04-15 15:05:22 +0800

    refactor the code of login

--- src/main/java/com/hirebigdata/spider/zhilian/main/CrawlZhiLian.java
+++ src/main/java/com/hirebigdata/spider/zhilian/main/CrawlZhiLian.java
@@ -1,90 +1,101 @@
 package com.hirebigdata.spider.zhilian.main;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import com.hirebigdata.spider.zhilian.config.ZhiLianConfig;
 import com.hirebigdata.spider.zhilian.utils.HttpUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 
 import com.ImagePreProcess3;
+
 /**
  * User: shellbye.com@gmail.com
  * Date: 2015/4/15
  */
 public class CrawlZhiLian {
-    HttpClient httpclient = HttpClientBuilder.create().build();
-    String headerString = "";
-    public static void main(String[] args) throws Exception{
-        new CrawlZhiLian().tryToLogin();
+    public static void main(String[] args) throws Exception {
+        CookieStore zhilianCookie = new BasicCookieStore();
+        System.out.println(login(zhilianCookie));
     }
 
-    public void tryToLogin() throws IOException {
+    public static int login(CookieStore cookieStore) {
         while (true) {
             long nowTimeStamp = System.currentTimeMillis();
             long nowTime = nowTimeStamp / 1000;
-            HttpPost httppost = new HttpPost(ZhiLianConfig.LOGIN_POST + "?DYWE="
-                    + nowTimeStamp + "." + 438478 + "." + nowTime + "."
-                    + nowTime + "." + 1);
-            httppost.setHeader("Referer", "http://hr.zhaopin.com/hrclub/index.html");
-            httppost.setHeader("Origin", "http://hr.zhaopin.com");
-            httppost.setHeader("Host", "rd2.zhaopin.com");
 
-            List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-            params.add(new BasicNameValuePair("username", "jiri59483132"));
-            params.add(new BasicNameValuePair("password", "linxiaohua87860519"));
 
-            // validate
-            String validate = getValidateCode(headerString);
-            params.add(new BasicNameValuePair("Validate", validate));
-            params.add(new BasicNameValuePair("Submit", ""));
-            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-            HttpResponse postResponse = httpclient.execute(httppost);
-            System.out.println(postResponse.toString());
-            if (postResponse.getStatusLine().getStatusCode() != 302) {
-                continue;
+            HashMap<String, String> param = new HashMap<>();
+            param.put("username", "jiri59483132");
+            param.put("password", "linxiaohua87860519");
+            param.put("Validate", getCode(cookieStore));
+            param.put("Submit", "");
+
+
+            HttpResponse postResponse = HttpUtils.doPostResponse(
+                    ZhiLianConfig.LOGIN_POST + "?DYWE="
+                            + nowTimeStamp + "." + 438478 + "."
+                            + nowTime + "."
+                            + nowTime + "." + 1,
+                    param,
+                    cookieStore
+            );
+
+
+            if (postResponse.getStatusLine().getStatusCode() == 302) {
+                Header[] header = postResponse.getHeaders("Location");
+                String location = header[0].getValue();
+                HttpResponse getResponse = HttpUtils.doGetResponse(location, new BasicCookieStore());
+                if (getResponse.getStatusLine().getStatusCode() == 200) {
+                    return 200;
+                }
             }
-            Header[] header = postResponse.getHeaders("Location");
-            String location = header[0].getValue();
-            httppost.releaseConnection();
-
-            HttpGet httpget = new HttpGet(location);
-            HttpResponse getResponse2 = httpclient.execute(httpget);
-            System.out.println(getResponse2.toString());
-            httpget.releaseConnection();
-            if (getResponse2.getStatusLine().getStatusCode() != 200) {
-                continue;
-            }
-            System.out.println("successfully login!");
-            return;
         }
     }
 
-    public File saveValidatePicture(HttpClient httpclient, String cookie) {
+    public static String getCode(CookieStore cookieStore) {
+        String line = "";
+        while (line.length() < 4) {
+            File f = savePicture(cookieStore);
+            try {
+                // 因为ImagePreProcess3的main函数是直接将结果打印了出来，
+                // 所以这里我对console的输出做了重定向，打印到文件中，
+                // 然后从文件中读取结果
+                File f2 = new File("c:\\output1.txt");
+                PrintStream out = new PrintStream(new FileOutputStream(f2));
+                PrintStream old_out = System.out;
+                System.setOut(out);
+                ImagePreProcess3.main((f.getAbsolutePath() + " c:\\train").split(" "));
+                System.setOut(old_out);
+                BufferedReader br2 = new BufferedReader(new FileReader(f2));
+                if ((line = br2.readLine()) != null)
+                    continue;
+
+                f2.delete();
+                f.delete();
+            } catch (Exception e) {
+                e.printStackTrace();
+                f.delete();
+                return null;
+            }
+        }
+        return line;
+    }
+
+    public static File savePicture(CookieStore cookieStore) {
         try {
             String a = ZhiLianConfig.VALIDATA_PICTURE + System.currentTimeMillis() / 1000L;
-            HttpGet httpgetNewPicture = new HttpGet(a);
-            HttpResponse getResponse = httpclient.execute(httpgetNewPicture);
+            HttpResponse getResponse = HttpUtils.doGetResponse(a, cookieStore);
             HttpEntity httpEntity = getResponse.getEntity();
-            Header header = getResponse.getHeaders("Set-Cookie")[0];
-            cookie += header.getValue().split(";")[0];
 
             byte[] b = new byte[1];
             DataInputStream di = new DataInputStream(httpEntity.getContent());
@@ -98,42 +109,5 @@ public class CrawlZhiLian {
         } catch (Exception e) {
             return null;
         }
-    }
-
-    public String getValidateCode(String cookie) {
-        String line = "";
-        while (line.length() < 4) {
-            File f = saveValidatePicture(httpclient, cookie);
-            try {
-                // 因为ImagePreProcess3的main函数是直接将结果打印了出来，
-                // 所以这里我对console的输出做了重定向，打印到文件中，
-                // 然后从文件中读取结果
-                String arg = f.getAbsolutePath() + " c:\\train";
-                File f2 = new File("c:\\output1.txt");
-                PrintStream out = new PrintStream(new FileOutputStream(f2));
-                PrintStream old_out = System.out;
-                System.setOut(out);
-                // get the code
-                ImagePreProcess3.main(arg.split(" "));
-                System.setOut(old_out);
-                BufferedReader br2 = new BufferedReader(new FileReader(f2));
-                if ((line = br2.readLine()) != null) {
-                    continue;
-                } else {
-                    line = "";
-                }
-                f2.delete();
-                f.delete();
-            } catch (IOException e) {
-                e.printStackTrace();
-                f.delete();
-                return null;
-            }catch (Exception e) {
-                e.printStackTrace();
-                f.delete();
-                return null;
-            }
-        }
-        return line;
     }
 }
